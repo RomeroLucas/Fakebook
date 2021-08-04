745ce4065b03cabdbd192470ba11ea62caff599c
Lucas Romero <lucas.romerovivas@gmail.com>
2021-08-04 13:13:43 -0300

    url do servidor

--- src/scripts.js
+++ src/scripts.js
@@ -1,4 +1,5 @@
 // AQUI ESTÃO ALGUNS SCRIPTS RECORRENTES NA APLICAÇÃO
 
 //URL PARA INTEGRAÇÃO COM O BANCO DE DADOS
+//  'http://localhost:4000'
 export const url = 'https://fakebooklucas.herokuapp.com'
