6f07bb843d67d5854deb2f94c38e97810cc95cdd
Lucas Romero <lucas.romerovivas@gmail.com>
2021-08-04 13:44:14 -0300

    anti span no post de usuarios

--- src/components/form/post.js
+++ src/components/form/post.js
@@ -8,7 +8,7 @@ import {url} from '../../scripts'
 
 export default function PostForm() {
     let userReducer = useSelector(state => state.user)
-    const [post, setpost] = useState(false)
+    const [post, setpost] = useState({span: 0})
     
     let handlechange = ({target}) => {
         let {name, value} = target
@@ -23,20 +23,22 @@ export default function PostForm() {
     let dispatch = useDispatch()
     let handlesubmit = (e) => {  
         e.preventDefault()
+        post.content && post.span < 2 ?
         axios.post(`${url}/publicar/id_user=${userReducer.id_user}`, post, {headers: headers})
         .then(() => {
             setpost(false)
             axios.get(`${url}/publicacoes`)
-            .then(response => dispatch({type: 'CARREGAR_POSTS', payload: response.data}))
+            .then(response => dispatch({type: 'CARREGAR_POSTS', payload: response.data}), setpost({...post, span: post.span + 1}))
         })
+        : alert('Por favor escreva alguma coisa!') 
     }
 
     return (
-        <div className='container-sm formpost-container'>
+        <form className='container-sm formpost-container'>
             <div className='mb-3'>
-            <textarea onChange={handlechange} name='content' id='userpub' className="form-control" rows='3' placeholder={`Diga o que está pensando ${userReducer.username}`}></textarea>
+            <textarea onChange={handlechange} name='content' id='userpub' className="form-control" rows='3' placeholder={`Diga o que está pensando ${userReducer.username}`} required={true}></textarea>
             <button onClick={handlesubmit} className='btn btn-primary form-control' id="btn-postar">POSTAR</button>
             </div>
-        </div>
+        </form>
     )
 }
